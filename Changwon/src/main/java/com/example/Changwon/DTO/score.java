794ce455d2a3f657e2c955bfd0dae73e1439e809
jh96code <whitev77@naver.com>
2022-10-25 14:52:30 +0900

    Create score.java

---
+++ Changwon/src/main/java/com/example/Changwon/DTO/score.java
@@ -0,0 +1,33 @@
+package com.example.Changwon.DTO;
+
+public class score {
+	int aid;
+	String name;
+	int score;
+	String questions;
+	
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getQuestions() {
+		return questions;
+	}
+	public void setQuestions(String questions) {
+		this.questions = questions;
+	}
+}
