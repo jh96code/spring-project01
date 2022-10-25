da9df37f0d2b7c2c7939c79851b7ce6725712711
jh96code <whitev77@naver.com>
2022-10-25 14:52:16 +0900

    Create cartDto.java

---
+++ Changwon/src/main/java/com/example/Changwon/DTO/cartDto.java
@@ -0,0 +1,35 @@
+package com.example.Changwon.DTO;
+
+public class cartDto {
+	
+	int cartId;
+	String cartName;
+	int cartPrice;
+	int cartStock;
+	
+	public int getCartId() {
+		return cartId;
+	}
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+	public String getCartName() {
+		return cartName;
+	}
+	public void setCartName(String cartName) {
+		this.cartName = cartName;
+	}
+	public int getCartPrice() {
+		return cartPrice;
+	}
+	public void setCartPrice(int cartPrice) {
+		this.cartPrice = cartPrice;
+	}
+	public int getCartStock() {
+		return cartStock;
+	}
+	public void setCartStock(int cartStock) {
+		this.cartStock = cartStock;
+	}
+
+}
