fb0421d63c09bfd1fbbf839fc3a61509c13e7171
jh96code <whitev77@naver.com>
2022-10-25 14:52:25 +0900

    Create memberDTO.java

---
+++ Changwon/src/main/java/com/example/Changwon/DTO/memberDTO.java
@@ -0,0 +1,111 @@
+package com.example.Changwon.DTO;
+
+public class memberDTO {
+	String id ;
+	String password;
+	String name ;
+	String gender ;
+	String year;
+	String month;
+	String day;
+	String birth;
+	String mail1;
+	String mail2; 
+	String mail;
+	String phone ;
+	String address ;
+	String regist_day;
+	Boolean admincheck;
+	
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	} 
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getMail1() {
+		return mail1;
+	}
+	public void setMail1(String mail1) {
+		this.mail1 = mail1;
+	}
+	public String getMail2() {
+		return mail2;
+	}
+	public void setMail2(String mail2) {
+		this.mail2 = mail2;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getRegist_day() {
+		return regist_day;
+	}
+	public void setRegist_day(String regist_day) {
+		this.regist_day = regist_day;
+	} 	
+	public Boolean getAdmincheck() {
+		return admincheck;
+	}
+	public void setAdmincheck(boolean admincheck) {
+		this.admincheck = admincheck;
+	}
+	
+}
