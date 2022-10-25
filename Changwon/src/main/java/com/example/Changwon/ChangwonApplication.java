99be617d61687f4dd4d6e76c3aca6bf81cc9758e
jh96code <whitev77@naver.com>
2022-10-25 14:51:28 +0900

    Create ChangwonApplication.java

---
+++ Changwon/src/main/java/com/example/Changwon/ChangwonApplication.java
@@ -0,0 +1,13 @@
+package com.example.Changwon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangwonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangwonApplication.class, args);
+	}
+
+}
