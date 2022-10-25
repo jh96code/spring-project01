862b5431ff82c14314c87222b62b8836097a6b3c
jh96code <whitev77@naver.com>
2022-10-25 14:55:59 +0900

    Create ChangwonApplicationTests.java

---
+++ Changwon/src/test/java/com/example/Changwon/ChangwonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Changwon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangwonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
